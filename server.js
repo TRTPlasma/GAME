8fe2604ebfe1b564276cac84143cafd5d2b2b462
TRTPlasma <90149725+TRTPlasma@users.noreply.github.com>
2021-09-05 21:59:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 const express = require('express')
+const cors = require('cors')
+const userRoutes = require('./routes/user.routes')
+
+require('dotenv').config({path: './config/.env'})
+
+const app = express()
+
+const corsOptions = {
+    origin: process.env.CLIENT,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('*', checkUser)
+app.use('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+})
+
+app.use('/user', userRoutes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on PORT ${process.env.PORT}`)
+})
